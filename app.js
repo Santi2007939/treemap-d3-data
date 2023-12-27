7814cda6deb2b2db5f9f1bb4d458db7634eb668d
Santi2007939 <117610911+Santi2007939@users.noreply.github.com>
2023-12-27 10:18:42 -0500

    Colors

--- app.js
+++ app.js
@@ -5,18 +5,56 @@ let ed;
 let cd;
 let canvas = d3.select("#canvas");
 
+let colors = [
+    "#7bd3f3",
+    "#54c8f3",
+    "#30ace2",
+    "#1e70b6",
+    "#0f599b",
+    "#0c3459",  
+    "#0e202e"
+  ]
+
 let map = () => {
-    
+    canvas.selectAll("path")
+            .data(cd)
+            .enter()
+            .append("path")
+            .attr("d", d3.geoPath())
+            .attr("class", "county")
+            .attr("fill", (i) => {
+                let id = i["id"];
+                let county = ed.find((i) => {
+                    return i["fips"] == id;
+                })
+                let number = county["bachelorsOrHigher"];
+                let lim = 12;
+                for (var i = 0; i <= 6; i++) {
+                    if (number <= lim) {
+                        return colors[i];
+                    }
+                    lim += 9;
+                }
+            })
+            .attr("data-fips", (i) => {return i["id"]})
+            .attr("data-education", (i) => {
+                let id = i["id"];
+                let county = ed.find((i) => {
+                    return i["fips"] == id;
+                })
+                return county["bachelorsOrHigher"];
+            })
 };
 
-fetch(urlED)
+fetch(urlCD)
     .then(response => response.json())
     .then(data => {
-        ed = data;
-        fetch(urlCD)
+        cd = topojson.feature(data, data.objects.counties).features;
+        console.log(cd);
+        fetch(urlED)
             .then(response => response.json())
             .then(dat => {
-                cd = dat;
+                ed = dat;
                 map();
             })
             .catch(err => console.error("Error fetching the information: ", err))
